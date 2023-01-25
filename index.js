01fd4dcd5ce2963d3f3e6913d0ce815d64bec42e
Saifur Rahman <118433585+devSaifur@users.noreply.github.com>
2023-01-25 10:13:05 +0600

    1.0
    My first JavaScript project

---
+++ index.js
@@ -0,0 +1,69 @@
+let player = {
+  name: "Akash",
+  chips: 145
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = (player.name + ": $" + player.chips)
+
+// let sumEl = document.querySelector("#sum-el") --> another way to get the element id
+
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1
+  if (randomCard > 10) {
+    return 10
+  } else if (randomCard === 1) {
+    return 11
+  } else {
+    return randomCard
+  }
+}
+
+function startGame() {
+  isAlive = true
+  let randomCard1 = getRandomCard()
+  let randomCard2 = getRandomCard()
+  cards = [randomCard1, randomCard2]
+  sum = randomCard1 + randomCard2
+  renderGame()
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to drow a new card?"
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You are out of the game!"
+    isAlive = false
+  }
+  messageEl.textContent = message
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    cardNew = getRandomCard()
+    sum += cardNew
+    cards.push(cardNew)
+    renderGame()
+  }
+}
+
+
+
+
+
